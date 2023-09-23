c584578439b5d4a3bd4a61dd25082254d82c093b
ArefShojaei <arefshojaei82dev@gmail.com>
2023-09-23 16:54:52 +0330

    ADD: add Env validator

---
+++ core/Validators/Env.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Core\Validators;
+
+/**
+ * @package
+ */
+use Core\Contracts\Interfaces\EnvValidator as EnvValidatorInterface;
+
+
+/**
+ * @desc Env Validator
+ * @class
+ * @implements {EnvValidatorInterface}
+ */
+class Env implements EnvValidatorInterface {
+    /**
+     * @desc declare the "checkFileExists" method for checking file
+     * @method
+     * @public
+     * @static
+     * @name checkFileExists
+     * @return {void}
+     */    
+    public static function checkFileExists(string $file): void {
+        # check for not exsitsing the file
+        if(!file_exists($file)) {
+            throw new \Exception("The Env file doesn't exists!");
+        }
+    }
+}
