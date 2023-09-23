22ec60c04984d80c5fe3f656e43a41649b99349f
ArefShojaei <arefshojaei82dev@gmail.com>
2023-09-23 16:52:00 +0330

    ADD: add Env Validator interface

---
+++ core/Contracts/Interfaces/EnvValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Core\Contracts\Interfaces;
+
+
+/**
+ * @desc declare contracts for Env Validator
+ * @interface
+ * @name EnvValidator
+ */
+interface EnvValidator {
+    /**
+     * @desc declare the checkFileExists method for checking file
+     * @method
+     * @public
+     * @static
+     * @name checkFileExists
+     * @return {void}
+     */    
+    public static function checkFileExists(string $file): void;
+}
