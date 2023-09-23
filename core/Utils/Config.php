5e18f695e892d8074c5d5953bc976a6c52f175bf
ArefShojaei <arefshojaei82dev@gmail.com>
2023-09-23 19:25:47 +0330

    ADD: add Config Utility

---
+++ core/Utils/Config.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Core\Utils;
+
+/**
+ * @package
+ */
+use Core\{
+    Contracts\Interfaces\Config\Config as ConfigInterface,
+    Validators\Config as Validator
+};
+
+
+/**
+ * @desc Config Utility
+ * @class
+ * @implements {ConfigInterface}
+ */
+class Config implements ConfigInterface {
+    /**
+     * @desc base path folder
+     * @prop
+     * @private
+     * @constant {string}
+     * @default
+     * @return {string}
+     */
+    private const BASE_PATH = "configs/";
+
+    /**
+     * @desc loaded config data like an Array from a config file in "config/{filename}.php"
+     * @prop
+     * @private
+     * @return {array}
+     */
+    private static $loadedConfigData;
+
+    /**
+     * @desc find the config file
+     * @method
+     * @public
+     * @static
+     * @name find
+     * @param {string} $filename - the config filename
+     * return {self}
+     */
+    public static function find(string $filename): self {
+        $file = self::BASE_PATH . $filename . ".php";
+        
+        Validator::checkFileExists($file);
+
+        self::$loadedConfigData = include $file;
+
+        return new self;
+    }
+
+    /**
+     * @desc get value of the loaded config data by key
+     * @method
+     * @public
+     * @static
+     * @name get
+     * @param {string} $key - key name
+     * @return {string}
+     */
+    public static function get(string $key): string {
+        return self::$loadedConfigData[$key];
+    }
+
+    /**
+     * @desc get all keys & values of the loaded config data
+     * @method
+     * @public
+     * @static
+     * @name all
+     * @return {array} 
+     */
+    public static function all(): array {
+        return self::$loadedConfigData;
+    }
+}
