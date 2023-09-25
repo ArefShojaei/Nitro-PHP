35994bbc793eb6458885cc5ccb73efb2531877c2
ArefShojaei <arefshojaei82dev@gmail.com>
2023-09-25 13:08:22 +0330

    ADD: add View validator Interface

---
+++ core/Contracts/Interfaces/View/ViewValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Core\Contracts\Interfaces\View;
+
+/**
+ * @desc declare contracts for View Validator 
+ * @interface
+ * @name ViewValidator
+ */
+interface ViewValidator {
+    /**
+     * @desc declare the "checkFileExists" method for checking file
+     * @method
+     * @public
+     * @static
+     * @name checkFileExists
+     * @param {string} $file - view( template engine file ) file
+     * @return {void}
+     */
+    public static function checkFileExists(string $file): void;
+}
