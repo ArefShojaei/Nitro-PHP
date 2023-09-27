949a286977308b46b68df7d30d663fa8721fee1f
ArefShojaei <arefshojaei82dev@gmail.com>
2023-09-27 18:09:19 +0330

    ADD: add Console config for the php nitro info in CMD

---
+++ configs/console.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @desc describe Command Line (php nitro)
+ * @return {array}
+ */
+return [
+    "lables" => [
+        "options" => [
+            "Power Cli",
+        ],
+        "commands" => [
+            "php artisan       " => "Welcome Message",
+            "php artisan welcome" => "Welcome Message",
+            "php artisan --version" => "PHP Version",
+            "php artisan env" => "Env Variables",
+        ],
+    ]
+];
