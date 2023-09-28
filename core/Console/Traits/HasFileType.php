823918adc43e2bb08471031ca419b3f5a5ae55b7
ArefShojaei <arefshojaei82dev@gmail.com>
2023-09-28 22:23:46 +0330

    ADD: add controller type for Make command

--- core/Console/Traits/HasFileType.php
+++ core/Console/Traits/HasFileType.php
@@ -15,7 +15,14 @@ trait HasFileType {
      * @param {string} $filename
      * @return {string}
      */
-    private function controller(string $filename): string {}
+    private function controller(string $filename): string {
+        $this->fileContent .= $this->addPhpTag() . $this->addBr(2);
+        $this->fileContent .= $this->addNamespace("App\Http\Controllers") . $this->addBr(2);
+        $this->fileContent .= $this->addUse("Core\Http\{ Request, Response }") . $this->addBr(3);
+        $this->fileContent .= $this->addClass($filename);
+        
+        return $this->fileContent;
+    }
 
     /**
      * @desc middleware file content
