789b5f8d7d4cfe84bc5cd26ee2d7e571fa61075d
ArefShojaei <arefshojaei82dev@gmail.com>
2023-10-03 13:05:43 +0330

    ADD: add Api interface

---
+++ core/Contracts/Interfaces/Api.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Core\Contracts\Interfaces;
+
+
+/**
+ * @desc declare contracts for Api http request
+ * @interface
+ * @name Api
+ */
+interface Api {
+    /**
+     * @desc declare the "get" method for the GET http request
+     * @method
+     * @public
+     * @name get
+     * @param {string} $endPoint - api end-point
+     * @return {void}
+     */
+    public function get(string $endPoint): object|array;
+    
+    /**
+     * @desc declare the "post" method for the POST http request
+     * @method
+     * @public
+     * @name post
+     * @param {string} $endPoint - api end-point
+     * @param {array} $data
+     * @return {void}
+     */
+    public function post(string $endPoint, array $data = []): object|array;
+    
+    /**
+     * @desc declare the "put" method for the PUT http request
+     * @method
+     * @public
+     * @name put
+     * @param {string} $endPoint - api end-point
+     * @param {array} $data
+     * @return {void}
+     */
+    public function put(string $endPoint, array $data = []): object|array;
+    
+    /**
+     * @desc declare the "patch" method for the PATCH http request
+     * @method
+     * @public
+     * @name patch
+     * @param {string} $endPoint - api end-point
+     * @param {array} $data
+     * @return {void}
+     */
+    public function patch(string $endPoint, array $data = []): object|array;
+    
+    /**
+     * @desc declare the "delete" method for the DELETE http request
+     * @method
+     * @public
+     * @name delete
+     * @param {string} $endPoint - api end-point
+     * @param {array} $data
+     * @return {void}
+     */
+    public function delete(string $endPoint, array $data = []): object|array;
+}
