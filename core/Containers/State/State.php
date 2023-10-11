1d0a5675a03360038137c4d2d78d7fd11557ac1d
ArefShojaei <arefshojaei82dev@gmail.com>
2023-10-11 20:42:46 +0330

    ADD: add State container

---
+++ core/Containers/State/State.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Core\Containers\State;
+
+/**
+ * @desc State container
+ * @class
+ */
+class State {
+    /**
+     * @desc states list
+     * @prop
+     * @private
+     * @type array
+     * @default
+     * @return {array}
+     */
+    private array $states = [];
+
+    
+    /**
+     * @desc set state
+     * @method
+     * @public
+     * @name setState
+     * @param {mixed} $data
+     * @return {void}
+     */
+    public function setState(mixed $data): void {
+        array_push($this->states, $data);
+    }
+
+    /**
+     * @desc get state
+     * @method
+     * @public
+     * @name getState
+     * @return {array}
+     */    
+    public function getState(): array {
+        return $this->states;
+    }
+}
