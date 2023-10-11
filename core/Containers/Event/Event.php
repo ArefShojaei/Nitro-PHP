3c9e5c0cbfc0f9887da088531e52a63e1ecd9972
ArefShojaei <arefshojaei82dev@gmail.com>
2023-10-11 20:42:29 +0330

    ADD: add Event container

---
+++ core/Containers/Event/Event.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Core\Containers\Event;
+
+/**
+ * @desc Event container
+ * @class
+ */
+class Event {
+    /**
+     * @desc events list
+     * @prop
+     * @private
+     * @type {array}
+     * @default
+     * @return {array}
+     */
+    private array $events = [];
+
+
+    /**
+     * @desc add event
+     * @method
+     * @public
+     * @name addListener
+     * @param {string} $event - event name
+     * @param {callable} $action - event action
+     * @return {void}
+     */
+    public function addListener(string $event, callable $action): void {
+        $this->events[$event] = $action;
+    }
+
+    /**
+     * @desc dispatch event
+     * @method
+     * @public
+     * @name dispatch
+     * @param {string} $event - event name
+     * @param {mixed} $data - event data
+     * @return {void}
+     */
+    public function dispatch(string $event, mixed $data): void {
+        $action = $this->events[$event];
+
+        $action($data);
+    }
+}
