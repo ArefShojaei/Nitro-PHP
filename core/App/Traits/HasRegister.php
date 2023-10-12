f08a6c7e5373e4ca162fd6e4f9632d78608093ed
ArefShojaei <arefshojaei82dev@gmail.com>
2023-10-12 16:25:11 +0330

    ADD: add HasRegister trait

--- core/App/Traits/HasRegister.php
+++ core/App/Traits/HasRegister.php
@@ -6,18 +6,29 @@
 namespace Core\App\Traits;
 
 
-trait HasRegsiter {
-    private function regsiterRoutes() {
-        // $this->web = require_once "routes/web.php";
-        // $this->api = require_once "routes/api.php";
+trait HasRegister {
+    /**
+     * @desc register IOC containers
+     * @method
+     * @public
+     * @name registerContainers
+     * @param {object} $app
+     * @return {void}
+     */
+    public function registerContainers($app): void {
+        $app->resolve("Logger");
+        $app->resolve("Router");
     }
 
-    private function regsiterProviders() {
-        $this->regsiterWebHooks();
-        $this->regsiterApiHooks();
+    /**
+     * @desc register routes (web & api)
+     * @method
+     * @private
+     * @name registerRoutes
+     * @return {void}
+     */
+    private function registerRoutes(): void {
+        require_once "routes/web.php";
+        require_once "routes/api.php";
     }
-
-    private function regsiterWebHooks() {}
-    
-    private function regsiterApiHooks() {}
 }
