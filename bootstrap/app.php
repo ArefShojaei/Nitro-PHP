9281f19239f7e649570ede4ced8be2cbb0765230
ArefShojaei <arefshojaei82dev@gmail.com>
2023-10-13 12:28:42 +0330

    REFACTOR: refactor proceeses for bootstraping the app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,55 +3,21 @@
 /**
  * @package
  */
-use Core\{
-    Routing\Router,
-    Utils\Env,
-    Logger\Logger,
-    App\App
-};
-use Core\Http\{ Request, Response };
-use Core\Containers\{
-    Event\Event,
-    State\State,
-    Ioc\Ioc,
-};
+use Core\App\App;
 
 
 # create an instance of the App
 $app = new App();
 
+# get object containers 
+$registerObjects = require_once "bootstrap/containers/objects.php";
 
-# use IOC container for binding objects
-$app->bind("Event", function($container) {
-    return new Event();
-});
+# get event containers
+$registerEvents = require_once "bootstrap/containers/events.php";
 
-$app->bind("Logger", function($container) {
-    return new Logger(["type" => "dev"]);
-});
-
-$app->bind("Request", function($container) {
-    return new Request();
-});
-
-$app->bind("Response", function($container) {
-    return new Response();
-});
-
-$app->singleton("Router", function($container) {
-    return Router::getInstance(
-        $container->resolve("Request"),
-        $container->resolve("Response")
-    );
-});
-
-
-# get Event object of the container objects
-$event = $app->resolve("Event");
-
-# disaptch new event for starting the app
-$event->dispatch("app.start", $app);
+# register object containers (IOC)
+$registerObjects($app);
 
 
 # return event object
-return $event;
+return $registerEvents($app);
