2dec2cb81bdb40f6274ed18d38eb33fc86b57d32
ArefShojaei <arefshojaei82dev@gmail.com>
2023-10-13 18:38:25 +0330

    ADD: add some comments

--- core/Utils/JWT.php
+++ core/Utils/JWT.php
@@ -51,16 +51,28 @@ class JWT implements JWT_Interface {
      * @return {string}
      */
     public static function sign(array $payload, string $secretKey): string {
+        # convert the header to JSON type
         $header = json_encode(self::HEADER);
+        
+        # encode the header to base64-url
         $headerBase64Url = self::encodeBase64Url($header);
         
+        
+        # convert the payload to JSON type
         $payload = json_encode($payload);
+
+        # encode the payload to base64-url
         $payloadBase64Url = self::encodeBase64Url($payload);
         
+
+        # generate hash value for the signature
         $signature = hash_hmac(self::ALG, "{$headerBase64Url}.{$payloadBase64Url}", $secretKey, true);
+        
+        # encode the signature to base64-url
         $signatureBase64Url = self::encodeBase64Url($signature);
 
 
+        # return token as string type
         return "{$headerBase64Url}.{$payloadBase64Url}.{$signatureBase64Url}";
     }
 
@@ -75,22 +87,29 @@ class JWT implements JWT_Interface {
      * @return {array|null}
      */
     public static function verify(string $token, string $secretKey): array|null {
+        # extract token parts
         list($headerBase64Url, $payloadBase64Url, $signatureBase64Url) = explode(".", $token);
 
+        # decode JSON & base64Url 
         $header = json_decode(self::decodeBase64Url($headerBase64Url));
         $payload = json_decode(self::decodeBase64Url($payloadBase64Url), true);
 
+        # validation for the header & payload
         if (!$header || !$payload) {
             return null;
         }
 
+        # decode the signature to base64
         $signature = self::decodeBase64Url($signatureBase64Url);
         $expectedSignature = hash_hmac(self::ALG, "{$headerBase64Url}.{$payloadBase64Url}", $secretKey, true);
 
+        # validation for the signature & expectedSignature
         if ($signature !== $expectedSignature) {
             return null;
         }
 
+        
+        # reutrn payload data
         return $payload;
     }
 
