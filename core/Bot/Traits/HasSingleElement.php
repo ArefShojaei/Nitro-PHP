44746cea488e560841b60d55a1e7a39605797cfe
ArefShojaei <arefshojaei82dev@gmail.com>
2023-10-14 21:37:02 +0330

    ADD: add HasSingleElement trait

---
+++ core/Bot/Traits/HasSingleElement.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Core\Bot\Traits;
+
+
+trait HasSingleElement {
+    /**
+     * @desc select an element
+     * @method
+     * @public
+     * @name count
+     * @param {string} $selector - DOM selector
+     * @return {self}
+     */
+    public function find(string $selector): self {
+        $this->element = $this->document->query($this->convertSelectorToXPath($selector));
+
+        return $this;
+    }
+
+    /**
+     * @desc get element attribute by name
+     * @method
+     * @public
+     * @name attr
+     * @param {string} $name
+     * @return {string}
+     */
+    public function attr(string $name): string {
+        return $this->element[0]->getAttribute($name);
+    }
+
+    /**
+     * @desc has element attrubute
+     * @method
+     * @public
+     * @name hasAttr
+     * @param {string} $name
+     * @return {boolean}
+     */
+    public function hasAttr(string $name): bool {
+        return $this->element->hasAttribute($name);
+    }
+}
