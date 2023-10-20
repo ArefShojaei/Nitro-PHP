e007e85faaeb2e8a5a0d9edee70da8b93e63be8e
ArefShojaei <arefshojaei82dev@gmail.com>
2023-10-20 10:05:24 +0330

    ADD: add Mysql driver

---
+++ core/Database/Drivers/Mysql.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @namespace
+ */
+namespace Core\Database\Drivers;
+
+/**
+ * @package
+ */
+use Core\Contracts\Interfaces\Mysql as MysqlInterface;
+
+
+/**
+ * @desc mysql driver
+ * @class
+ */
+class Mysql implements MysqlInterface {
+    private static $instance;
+    private $connection;
+
+
+    private function __construct() {
+        try {
+            $this->connection = new \PDO("mysql:host=localhost;dbname=sample;charset=utf8", "root", "", [\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_OBJ]);
+        } catch (\PDOException $error) {
+            die("Database connection was failed: " . $error->getMessage());
+        }
+    }
+
+    public static function getInstance(): self {
+        if(!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection(): object {
+        return $this->connection;
+    }
+}
